cc613aade1e26fd1f0c01fffe551b02ef8e91bc3
tanacchi <q111026d@mail.kyutech.jp>
2018-06-05 19:56:43 +0900

    Pass test for 'user#signin'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
+  @is_login = false
   def top
+  end
+  
+  def signin
+    @is_login = true
+    redirect_to :action => 'top'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home/top'
+  get 'home/signin'
   get 'user/show'
   root 'home#top'
 end
