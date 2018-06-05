d70c5cbfe7611644594cfe2354b662a540453539
tanacchi <q111026d@mail.kyutech.jp>
2018-06-05 18:49:06 +0900

    Add top page of home

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should top page exist" do
+    get home_top_url
+    assert_response :success
+  end
 end
