25d9343852d91218e225f95d7bb30bbebddb3f7b
tanacchi <q111026d@mail.kyutech.jp>
2018-06-06 00:08:45 +0900

    Remove unnecessary valiable

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,12 @@
 class HomeController < ApplicationController
-  @is_login = false
   def top
   end
   
   def signin
-    @is_login = true
     redirect_to root_url
   end
 
   def signout
-    @is_login = false
     redirect_to root_url
   end
 end
