a2a1906f64666efcf2d7128c0e73047e9affb7bf
tanacchi <q111026d@mail.kyutech.jp>
2018-06-05 20:24:44 +0900

    Pass test for 'home#signout'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,4 +7,9 @@ class HomeController < ApplicationController
     @is_login = true
     redirect_to :action => 'top', :is_login => true
   end
+
+  def signout
+    @is_login = false
+    redirect_to :action => 'top', :is_login => false
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home/top'
   get 'home/signin'
+  get 'home/signout'
   get 'user/show'
   root 'home#top'
 end
