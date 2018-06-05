bba08eee3df951d8eb3fc4459f5ce80dbfa459fd
tanacchi <q111026d@mail.kyutech.jp>
2018-06-06 02:27:28 +0900

    Fix test for new signin page

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserControllerTest < ActionDispatch::IntegrationTest
   test "should users home exits" do
-    get user_show_url
+    get user_url
     assert_response :success
   end
 end
