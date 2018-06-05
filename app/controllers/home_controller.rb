2f867f43ccef41479608d52f153c774e7b20c11c
tanacchi <q111026d@mail.kyutech.jp>
2018-06-05 23:13:12 +0900

    Modify from '=>' to ':'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,15 +5,15 @@ class HomeController < ApplicationController
   
   def signin
     @is_login = true
-    redirect_to :action => 'top', :is_login => true
+    redirect_to action: 'top', is_login: true
   end
 
   def signout
     @is_login = false
-    redirect_to :action => 'top', :is_login => false
+    redirect_to action: 'top', is_login: false
   end
 
   def signup
-    redirect_to :controller => 'user', :action => 'new'
+    redirect_to controller: 'user', action: 'new'
   end
 end
