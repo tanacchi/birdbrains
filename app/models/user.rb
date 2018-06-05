57dae7ae790230b3baee54493d27990da80498d4
tanacchi <q111026d@mail.kyutech.jp>
2018-06-05 21:06:58 +0900

    Pass the test for unique email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { in: 3..20 }
+  validates :name,
+            presence: true, length: { in: 3..20 },
+            uniqueness: true
 end
