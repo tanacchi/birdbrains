76648d8ad91bbbabd766e50a0a0664998b29ee31
tanacchi <q111026d@mail.kyutech.jp>
2018-06-05 20:41:02 +0900

    Add test for new user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: 'Example User', email: 'example@mail.jp')
+  end
+
+  test "should new user valid" do
+    assert @user.valid?
+  end
 end
