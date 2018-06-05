eb37d7c4f4fdc663c60d69b9ea159c1b1f8a61cd
tanacchi <q111026d@mail.kyutech.jp>
2018-06-05 19:42:34 +0900

    Add test for 'user#signin'

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should top page exist" do
+  test "should top page exists" do
     get home_top_url
+    assert_response :success
+  end
+
+  test "should sign in page exists" do
+    get home_signin_url
     assert_response :success
   end
 end
