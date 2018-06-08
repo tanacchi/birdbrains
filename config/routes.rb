5c274217fc8429f856b359de1ec8975b585a0e32
tanacchi <q111026d@mail.kyutech.jp>
2018-06-08 19:30:20 +0900

    Modify route for signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  get  '/signup',   to: 'home#signup'
+  post '/signup',   to: 'users#create'
   get  '/signin',   to: 'home#signin'
   post '/signin',   to: 'home#check_user'
   get  '/signout',  to: 'home#signout'
-  get  '/signup',   to: 'users#new'
-  post 'users',     to: 'users#create'
   get  'users',     to: 'users#show'
   root 'home#top'
   resources :users
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,11 +6,16 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should sign up function works" do
+    get signup_url
+    assert_response :success
+  end
+
   test "should sign in function works" do
     get signin_url
     assert_response :success
   end
-
+  
   test "should sign out function works" do
     get signout_url
     assert_response :redirect
