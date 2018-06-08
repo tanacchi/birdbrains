e41af9f09bedcb60f79ef05142d7a558eacf0bc6
tanacchi <q111026d@mail.kyutech.jp>
2018-06-08 19:53:27 +0900

    Replace signup method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,12 @@ class UsersController < ApplicationController
     @all_user = User.all
   end
 
-  def new
-    @user = User.new
-  end
-
   def create
     @user = User.new(user_params)
     if @user.save
       redirect_to root_path
     else
-      render 'new'
+      redirect_to signup_path
     end
   end
   
