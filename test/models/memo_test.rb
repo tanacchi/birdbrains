c6ec81133c2f436c0a9b06b0aa99191a8c1d9f92
tanacchi <q111026d@mail.kyutech.jp>
2018-06-08 21:47:35 +0900

    Add test of relation

--- test/models/memo_test.rb
+++ test/models/memo_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class MemoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @memo = Memo.new(title: 'Example Memo', body: 'Example text',
+                     user_id: 1, made_time: DateTime.now, updated_time: DateTime.now)
+  end
+
+  test "should be valid" do
+    assert @memo.valid?
+  end
+  
+  test "user_id should not be nil" do
+    @memo.user_id = nil
+    assert_not @memo.valid?
+  end
+
+  test "should belong to a present user" do
+    @memo.user_id = 1145141919810
+    assert_not @memo.valid?
+  end
 end
