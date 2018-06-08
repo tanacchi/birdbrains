3beb104158e4f669a4864286058e9c1acc99871d
tanacchi <q111026d@mail.kyutech.jp>
2018-06-08 21:11:20 +0900

    Migrate memo database

--- db/migrate/20180608120400_create_memos.rb
+++ db/migrate/20180608120400_create_memos.rb
@@ -1,10 +1,11 @@
 class CreateMemos < ActiveRecord::Migration[5.2]
   def change
     create_table :memos do |t|
-      t.string :title
-      t.text :body
-
-      t.timestamps
+      t.string     :title
+      t.text       :body
+      t.references :user, foreign_key: true
+      t.datetime   :made_time
+      t.datetime   :updated_time
     end
   end
 end
