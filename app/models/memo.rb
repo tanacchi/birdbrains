582a888d81b8c14eafb0d5ac51d496f8decedb43
tanacchi <q111026d@mail.kyutech.jp>
2018-06-11 03:18:13 +0900

    Add method for made_time & updated_time

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,4 +1,16 @@
 class Memo < ApplicationRecord
   validates :title, length: {maximum: 30}
   belongs_to :user
+
+  before_create :set_made_time
+  before_save :set_updated_time
+
+  private
+  def set_made_time
+    self.made_time = DateTime.now
+  end
+
+  def set_updated_time
+    self.updated_time = DateTime.now
+  end
 end
