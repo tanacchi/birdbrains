c38605892e7175c499f9bf21881492d7d9f56828
tanacchi <q111026d@mail.kyutech.jp>
2018-06-11 22:23:18 +0900

    Add test for users#create

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,12 @@
 require 'test_helper'
 
 class UserControllerTest < ActionDispatch::IntegrationTest
+  # test "show page should work" do
+  #   assert true 
+  # end
+
+  test "user creater work" do
+    post users_path :user => {:name => 'test_name', :email => 'test@test'}
+    assert_response :redirect
+  end
 end
