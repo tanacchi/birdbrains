e58cfd93434f94295a686f474cb66a6d471581dc
tanacchi <q111026d@mail.kyutech.jp>
2018-06-11 21:38:40 +0900

    Pass the test for memo model

--- test/controllers/memos_controller_test.rb
+++ test/controllers/memos_controller_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class MemosControllerTest < ActionDispatch::IntegrationTest
   test "show page should be exist" do
-    get memos_url(id: 1)
+    get users_memos_url(id: 1)
     assert_response :success
   end
 
   test "new page should be exist" do
-    get memos_new_url
+    get new_users_memos_url
     assert_response :success
   end
 end
