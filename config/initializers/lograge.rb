cfc30fa976f486059d1cca11bacfea78256fcce1
tanacchi <q111026d@mail.kyutech.jp>
2018-06-15 18:50:18 +0900

    Modify app controller & lograge.rb for logger

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -5,12 +5,24 @@ Rails.application.configure do
   config.lograge.keep_original_rails_log = true
   config.lograge.formatter = Lograge::Formatters::Logstash.new
 
-  config.lograge.custom_options = lambda do |event|
-    {
-      host:      event.payload[:host],
-      username:  event.payload[:username],
-      time:      event.time,
-      timestamp: event.time
+  config.lograge.custom_options = lambda do |event|    
+    exceptions = ['controller', 'action', 'format', 'authenticity_token']
+    data = {
+      level:      'info',
+      host:       event.payload[:host],
+      ip:         event.payload[:ip],
+      login_id:   event.payload[:login_id],
+      username:   event.payload[:username],
+      user_agent: event.payload[:user_agent],
+      referer:    event.payload[:referer],
+      time:       event.time,
+      params:     event.payload[:params].except(*exceptions)
     }
+    if event.payload[:exception] # FIX: It cannot log exceptions
+      data[:level]               = 'fatal'
+      data[:exception]           = event.payload[:exception]
+      data[:exception_backtrace] = event.payload[:exception_object].backtrace[0..6]
+    end
+    data
   end
 end
