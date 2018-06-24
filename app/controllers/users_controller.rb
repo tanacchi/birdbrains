96834280a0409f76ff7158d12c59c6063178b486
tanacchi <q111026d@mail.kyutech.jp>
2018-06-24 19:36:12 +0900

    Add update action to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,15 @@ class UsersController < ApplicationController
   def edit
     @user = current_user
   end
+
+  def update
+    @user = current_user
+    if @user.update_attributes(user_params)
+      redirect_to users_url
+    else
+      redirect_back
+    end
+  end
   
   private
   def user_params
