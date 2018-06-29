1d95c593500fc56b249f46717a9d2140b99f01fb
tanacchi <q111026d@mail.kyutech.jp>
2018-06-29 20:08:17 +0900

    Add controller test for memos show action

--- test/controllers/memos_controller_test.rb
+++ test/controllers/memos_controller_test.rb
@@ -3,6 +3,19 @@ require 'test_helper'
 class MemosControllerTest < ActionDispatch::IntegrationTest
   def setup
     @memo = memos(:memo0)
+    @user = @memo.user
     @memo.save!
+  end
+
+  test "memos show page should be accessible" do
+    login @user
+    get users_memos_path(id: @memo)
+    assert_response :success
+  end
+
+  test "memos show page should not be accessible without login" do
+    get users_memos_path(id: @memo)
+    assert_response :redirect
+    assert_redirected_to signin_path
   end
 end
