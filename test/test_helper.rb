19df3600f88b118a75676806ca0978ea80f48de9
tanacchi <q111026d@mail.kyutech.jp>
2018-06-29 19:03:44 +0900

    Add login and logout method in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,8 +5,13 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def login(user)
+    post signin_path(session: { name: user.name, email: user.email })
+  end
+
+  def logout
+    get signout_path
+  end
 end
