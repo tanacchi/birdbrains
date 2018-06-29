d4c13503d456b65282485c630c59d04fc21174d9
tanacchi <q111026d@mail.kyutech.jp>
2018-06-29 19:13:12 +0900

    Add users controller test around show action

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,15 +4,21 @@ class UserControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:user0)
     @user.save!
-    login @user
   end
 
   def teardown
     logout
   end
 
-  test "user creater should work" do
-    post users_path :user => {:name => 'test_name', :email => 'test@test'}
+  test "users show page should be accessible" do
+    login @user
+    get users_path
+    assert_response :success
+  end
+
+  test "users show page should not be accessible without login" do
+    get users_path
     assert_response :redirect
+    assert_redirected_to signin_path
   end
 end
