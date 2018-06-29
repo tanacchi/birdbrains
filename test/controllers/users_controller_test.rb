75696e471c1a783cfcacec3c720a4d7e4a91a50b
tanacchi <q111026d@mail.kyutech.jp>
2018-06-29 19:04:36 +0900

    Add setup and teardown method in users controller test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,16 @@
 require 'test_helper'
 
 class UserControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:user0)
+    @user.save!
+    login @user
+  end
+
+  def teardown
+    logout
+  end
+
   test "user creater should work" do
     post users_path :user => {:name => 'test_name', :email => 'test@test'}
     assert_response :redirect
