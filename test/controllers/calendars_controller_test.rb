26cdfc91c29f98aab3c348075a50e40c5c7a20e3
tanacchi <q111026d@mail.kyutech.jp>
2018-06-29 20:42:17 +0900

    Add controller test for calendars index action

--- test/controllers/calendars_controller_test.rb
+++ test/controllers/calendars_controller_test.rb
@@ -1,4 +1,20 @@
 require 'test_helper'
 
 class CalendarsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:user0)
+    @user.save!
+  end
+  
+  test "calendars index page should be accessible" do
+    login @user
+    get calendars_index_path
+    assert_response :success
+  end
+
+  test "calendars index page should not be accessible without login" do
+    get calendars_index_path
+    assert_response :redirect
+    assert_redirected_to signin_path
+  end
 end
