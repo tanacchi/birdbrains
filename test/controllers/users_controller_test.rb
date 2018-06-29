dda1645612d7ea31ac26c3c9833d112b6fde2ed1
tanacchi <q111026d@mail.kyutech.jp>
2018-06-29 19:26:46 +0900

    Rename tests for users edit action

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -38,13 +38,13 @@ class UserControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to signup_path
   end
 
-  test "users edit page should accessible" do
+  test "users edit page should be accessible" do
     login @user
     get edit_users_path
     assert_response :success
   end
 
-  test "users edit page should not accessible without login" do
+  test "users edit page should not be accessible without login" do
     get edit_users_path
     assert_response :redirect
     assert_redirected_to signin_path
