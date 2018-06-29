9ba372e861098b9d2a11a0ed40653235b2cc7734
tanacchi <q111026d@mail.kyutech.jp>
2018-06-29 19:46:58 +0900

    Add setup method on memos controller test

--- test/controllers/memos_controller_test.rb
+++ test/controllers/memos_controller_test.rb
@@ -1,15 +1,8 @@
 require 'test_helper'
 
 class MemosControllerTest < ActionDispatch::IntegrationTest
-  test "memos show page should not be accessed without login" do
-    @memos = memos(:memo0)
-    get users_memos_path(@memos)
-    assert_response :redirect
-    assert_redirected_to signin_path
-  end
-
-  test "new page should be exist" do
-    get new_users_memos_path
-    assert_response :success
+  def setup
+    @memo = memos(:memo0)
+    @memo.save!
   end
 end
