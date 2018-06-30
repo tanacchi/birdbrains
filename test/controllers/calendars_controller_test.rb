92c1a1f376d677e867a6e91880992d6b89c67214
tanacchi <q111026d@mail.kyutech.jp>
2018-06-30 14:12:29 +0900

    Add template assertion for calendars controller

--- test/controllers/calendars_controller_test.rb
+++ test/controllers/calendars_controller_test.rb
@@ -9,11 +9,13 @@ class CalendarsControllerTest < ActionDispatch::IntegrationTest
   test "calendars index page should be accessible" do
     login @user
     get calendars_index_path
+    assert_template :index
     assert_response :success
   end
 
   test "calendars index page should not be accessible without login" do
     get calendars_index_path
+    assert_template nil
     assert_response :redirect
     assert_redirected_to signin_path
   end
