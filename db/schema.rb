0a163784fa1510d554244af0ea8a470515631610
tanacchi <q111026d@mail.kyutech.jp>
2018-08-26 12:35:41 +0900

    Create table of reminders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_06_074147) do
+ActiveRecord::Schema.define(version: 2018_08_26_032811) do
 
   create_table "memos", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 2018_07_06_074147) do
     t.integer "user_id"
     t.datetime "created_at"
     t.index ["user_id"], name: "index_notices_on_user_id"
+  end
+
+  create_table "reminders", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reminders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
